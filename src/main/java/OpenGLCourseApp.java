f33ea0a3c8ae455bce3f7caa7dea8813ea01b24e
iImmersao <philippatchin@gmail.com>
2023-05-20 00:08:01 -0400

    Finish clean-up of code for Section 2.

--- src/main/java/OpenGLCourseApp.java
+++ src/main/java/OpenGLCourseApp.java
@@ -1,48 +1,17 @@
 import org.joml.Matrix4f;
-import org.joml.Vector3f;
-import org.lwjgl.Version;
 import org.lwjgl.glfw.*;
 import org.lwjgl.opengl.*;
-import org.lwjgl.system.*;
 
-import java.nio.*;
 import java.util.LinkedList;
 import java.util.List;
 
-import static org.lwjgl.glfw.Callbacks.*;
-import static org.lwjgl.glfw.GLFW.*;
 import static org.lwjgl.opengl.GL11.*;
-import static org.lwjgl.opengl.GL15.*;
 import static org.lwjgl.opengl.GL20.*;
-import static org.lwjgl.opengl.GL30.glBindVertexArray;
 import static org.lwjgl.opengl.GL30C.glGenVertexArrays;
-import static org.lwjgl.system.MemoryStack.*;
-import static org.lwjgl.system.MathUtil.*;
-import static org.lwjgl.system.MemoryUtil.NULL;
 
 //import static org.joml.Matrix4f;
 
 public class OpenGLCourseApp {
-    final static int WIDTH = 800;
-    final static int HEIGHT = 600;
-
-    final static float TO_RADIANS = 3.14159265f / 180.0f;
-
-    static int VAO, VBO, IBO;
-    static int shader;
-    static int uniformModel, uniformProjection;
-
-    static boolean direction = true;
-    static float triOffset = 0.0f;
-    final static float triMaxoffset = 0.7f;
-    final static float triIncrement = 0.005f;
-
-    static float curAngle = 0.0f;
-
-    static boolean sizeDirection = true;
-    static float curSize = 0.4f;
-    final static float maxSize = 0.8f;
-    final static float minSize = 0.1f;
 
     // Vertex Shader
     final static String vShader = "shader.vert";
@@ -54,7 +23,9 @@ public class OpenGLCourseApp {
 
     private List<Shader> shaderList = new LinkedList<>();
 
-    void CreateTriangle() {
+    private Window mainWindow;
+
+    void CreateObjects() {
         /*
          */
         int indices[] = {
@@ -87,74 +58,19 @@ public class OpenGLCourseApp {
         shaderList.add(shader1);
     }
     public void run() {
+        mainWindow = new Window(800, 600);
+        mainWindow.initialise();
 
-        if (!GLFW.glfwInit()) {
-            System.out.println("GLFW initialisation failed!");
-            System.exit(1);
-        }
-
-        GLFW.glfwWindowHint(GLFW.GLFW_CONTEXT_VERSION_MAJOR, 3);
-        GLFW.glfwWindowHint(GLFW.GLFW_CONTEXT_VERSION_MINOR, 3);
-
-        GLFW.glfwWindowHint(GLFW.GLFW_OPENGL_PROFILE, GLFW.GLFW_OPENGL_CORE_PROFILE);
-
-        GLFW.glfwWindowHint(GLFW.GLFW_OPENGL_FORWARD_COMPAT, GLFW.GLFW_TRUE);
-
-        long mainWindow = GLFW.glfwCreateWindow(WIDTH, HEIGHT, "Test Window", NULL, NULL);
-        if (mainWindow == NULL) {
-            System.out.println("GLFW window creation failed!");
-            GLFW.glfwTerminate();
-            System.exit(2);
-        }
-
-        int[] bufferWidth = new int[1];
-        int[] bufferHeight = new int[1];
-        GLFW.glfwGetFramebufferSize(mainWindow, bufferWidth, bufferHeight);
-
-        GLFW.glfwMakeContextCurrent(mainWindow);
-
-        // This line is essential! Without it, the native code crashes.
-        GL.createCapabilities();
-
-        glEnable(GL_DEPTH_TEST);
-
-        GL33.glViewport(0, 0, bufferWidth[0], bufferHeight[0]);
-
-        CreateTriangle();
+        CreateObjects();
         CreateShaders();;
 
+        int uniformModel, uniformProjection;
+
         Matrix4f projection = new Matrix4f();
-        projection.setPerspective(45.0f, (float)bufferWidth[0]/(float)bufferHeight[0], 0.1f, 100.0f);
+        projection.setPerspective(45.0f, (float)mainWindow.getBufferWidth()/(float)mainWindow.getBufferHeight(), 0.1f, 100.0f);
 
-        while (!GLFW.glfwWindowShouldClose(mainWindow)) {
+        while (!mainWindow.getShouldClose()) {
             GLFW.glfwPollEvents();
-
-            if (direction) {
-                triOffset += triIncrement;
-            }
-            else {
-                triOffset -= triIncrement;
-            }
-
-            if (Math.abs(triOffset) >= triMaxoffset) {
-                direction = !direction;
-            }
-
-            curAngle += 0.1f;
-            if (curAngle >= 360) {
-                curAngle -= 360;
-            }
-
-            if (sizeDirection) {
-                curSize += 0.001f;
-            }
-            else {
-                curSize -= 0.001f;
-            }
-
-            if (curSize >= maxSize || curSize <= minSize) {
-                sizeDirection = !sizeDirection;
-            }
 
             GL33.glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
             GL33.glClear(GL33.GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
@@ -165,28 +81,25 @@ public class OpenGLCourseApp {
             uniformProjection = shader.getUniformProjection();
 
             Matrix4f model = new Matrix4f();
-            //model = model.rotate(curAngle * TO_RADIANS, 0.0f, 1.0f, 0.0f);
-            model = model.translate(triOffset, 0.0f, -2.5f);
+            model = model.translate(0.0f, 0.0f, -2.5f);
             model = model.scale(0.4f, 0.4f, 1.0f);
 
             float[] modelArr = new float[16];
             glUniformMatrix4fv(uniformModel, false, model.get(modelArr));
-            /*
-             */
             float[] perspectiveArr = new float[16];
             glUniformMatrix4fv(uniformProjection, false, projection.get(perspectiveArr));
 
             meshList.get(0).renderMesh();
 
             model = new Matrix4f();
-            model = model.translate(-triOffset, 1.0f, -2.5f);
+            model = model.translate(0.0f, 1.0f, -2.5f);
             model = model.scale(0.4f, 0.4f, 1.0f);
             glUniformMatrix4fv(uniformModel, false, model.get(modelArr));
             meshList.get(1).renderMesh();
 
             glUseProgram(0);
 
-            GLFW.glfwSwapBuffers(mainWindow);
+            mainWindow.swapBuffers();
         }
     }
 
--- src/main/java/Window.java
+++ src/main/java/Window.java
@@ -1,2 +1,65 @@
+import org.lwjgl.glfw.GLFW;
+import org.lwjgl.opengl.GL;
+import org.lwjgl.opengl.GL33;
+
+import static org.lwjgl.glfw.GLFW.glfwSwapBuffers;
+import static org.lwjgl.glfw.GLFW.glfwWindowShouldClose;
+import static org.lwjgl.opengl.GL11.GL_DEPTH_TEST;
+import static org.lwjgl.opengl.GL11.glEnable;
+import static org.lwjgl.system.MemoryUtil.NULL;
+
 public class Window {
+    private int width = 800;
+    private int height = 600;
+    private long mainWindow;
+    private int[] bufferWidth = new int[1];
+    private int[] bufferHeight = new int[1];
+
+    public Window(int windowWidth, int windowHeight) {
+        this.width = windowWidth;
+        this.height = windowHeight;
+    }
+
+    public int initialise() {
+        if (!GLFW.glfwInit()) {
+            System.out.println("GLFW initialisation failed!");
+            System.exit(1);
+        }
+
+        GLFW.glfwWindowHint(GLFW.GLFW_CONTEXT_VERSION_MAJOR, 3);
+        GLFW.glfwWindowHint(GLFW.GLFW_CONTEXT_VERSION_MINOR, 3);
+
+        GLFW.glfwWindowHint(GLFW.GLFW_OPENGL_PROFILE, GLFW.GLFW_OPENGL_CORE_PROFILE);
+
+        GLFW.glfwWindowHint(GLFW.GLFW_OPENGL_FORWARD_COMPAT, GLFW.GLFW_TRUE);
+
+        mainWindow = GLFW.glfwCreateWindow(width, height, "Test Window", NULL, NULL);
+        if (mainWindow == NULL) {
+            System.out.println("GLFW window creation failed!");
+            GLFW.glfwTerminate();
+            System.exit(2);
+        }
+
+        GLFW.glfwGetFramebufferSize(mainWindow, bufferWidth, bufferHeight);
+
+        GLFW.glfwMakeContextCurrent(mainWindow);
+
+        // This line is essential! Without it, the native code crashes.
+        GL.createCapabilities();
+
+        glEnable(GL_DEPTH_TEST);
+
+        GL33.glViewport(0, 0, bufferWidth[0], bufferHeight[0]);
+
+        return 0;
+    }
+
+    public int getBufferWidth() { return bufferWidth[0]; }
+
+    public int getBufferHeight() { return bufferHeight[0]; }
+
+    public boolean getShouldClose() { return glfwWindowShouldClose(mainWindow); }
+
+    public void swapBuffers() { glfwSwapBuffers(mainWindow); }
+
 }
